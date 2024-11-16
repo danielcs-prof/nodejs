fdd409bfcd8f07a438d81e2acbe5aed4edb47715
Daniel Corrêa da Silva <danielcs80@gmail.com>
2024-11-16 03:39:04 +0000

    Projeto inicial

---
+++ src/ex01.js
@@ -0,0 +1,6 @@
+const div = document.getElementById('app')
+const header = document.createElement('h1')
+const texto = 'Olá mundo !!'
+const contentHeader = document.createTextNode(texto)
+header.appendChild(contentHeader)
+app.appendChild(header)
